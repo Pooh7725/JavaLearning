3962c3ea07b5fc2996792c663409d674c3355806
Pooh7725 <116027278+Pooh7725@users.noreply.github.com>
2023-08-20 21:20:21 +0530

    Create CountOccuranceOfWords.java

---
+++ CountOccuranceOfWords.java
@@ -0,0 +1,35 @@
+package HashsetAndHashmap;
+
+import java.util.HashMap;
+
+public class CountOccuranceOfWords {
+    public static void main(String[] args) {
+        String str= "anu poo poo anu papa";
+        HashMap<String,Integer> map = new HashMap<>();
+        int count = 1;
+        String[]arr = str.split(" ");
+        for(int i=0; i<arr.length;i++){
+            if(!map.containsKey(arr[i])){
+                map.put(arr[i],count);
+            }
+            else{
+                map.put(arr[i],map.get(arr[i])+1);
+            }
+
+    }
+        System.out.println(map);
+//    public static HashMap<String,Integer>Occurance(String str){
+//        HashMap<String,Integer> map = new HashMap<>();
+//        int count = 1;
+//        String[]arr = str.split(" ");
+//        for(int i=0; i<arr.length;i++){
+//            if(!map.containsKey(arr[i])){
+//                map.put(arr[i],count);
+//            }
+//            else{
+//                map.put(arr[i],map.get(arr[i])+1);
+//            }
+//        }
+//        return map;
+    }
+}
